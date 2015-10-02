4788d6a64fcb046c3416aedf19e5bd579499cee6
Desislava Stoyanova <dstoyanova@Desislavas-MacBook-Air.local>
2015-10-02 17:46:35 +0200

    Initial commit

--- Quiz-1/main.cpp
+++ Quiz-1/main.cpp
@@ -7,9 +7,65 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+int* buffer;
+
+void sum(int* array,size_t size,int index) {
+    buffer[index] = 0;
+    for (int i=0; i<size; i++) {
+        buffer[index] += array[i];
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Create the matrix
+    cout << "Enter n: ";
+    size_t n; cin >> n;
+    
+    int** matrix = new int*[n];
+    for (int i=0; i<n; i++) {
+        matrix[i] = new int[n];
+    }
+    
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            cout << "matrix[" << i << "][" << j << "] = ";
+            cin >> matrix[i][j];
+        }
+    }
+    
+    // Creating threads
+    buffer = new int[n];
+    vector<thread> threads;
+    
+    for (int i=0; i<n; i++) {
+        threads.push_back(thread(sum,matrix[i],n,i));
+    }
+
+    for (auto &t : threads) {
+        t.join();
+    }
+    
+    // Get the final result
+    int result = 0;
+
+    for (int i=0; i<n; i++)
+        result += buffer[i];
+    
+    cout << result << endl;
+    
+    // Deallocating the memory
+    delete [] buffer;
+    
+    for (int i=0; i<n; i++) {
+        delete [] matrix[i];
+    }
+    
+    delete [] matrix;
+    
     return 0;
 }
